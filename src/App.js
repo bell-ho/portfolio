e30c9c3b26aa054efb0bff14b921fe405dc87868
jongho <jh01010@doctorsoft.co.kr>
2022-12-29 13:22:54 +0900

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useCallback, useState } from 'react';
 function App() {
   const [value, setValue] = useState(0);
 
-  return <div>리액트</div>;
+  return <div>리액트??</div>;
 }
 
 export default App;
