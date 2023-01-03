25b80f3092f95170f2be369ddae33485ad538966
bell-ho <jj0101065@gmail.com>
2023-01-03 13:11:10 +0900

    contact 작업

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
+
 const Contact = () => {
   return (
     <Box className={'section contact'}>
-      <Typography variant={'h1'} className={'contact__title'}>
-        Let's talk
-      </Typography>
-      <Typography variant={'h2'} className={'contact__email'}>
-        aaa@aaa.com
-      </Typography>
+      <h1 className={'contact__title'}>Let's talk</h1>
+      <h2 className={'contact__email'}>aaa@aaa.com</h2>
       <Box className={'contact__links'}>
         <a href={'#'} target={'_blank'}>
           <GitHubIcon />
         </a>
-        <Typography variant={'caption'} className={'contact__rights'}>
-          GOOD
-        </Typography>
+        <p className={'contact__rights'}>All rights</p>
       </Box>
     </Box>
   );
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,60 +1,60 @@
-import React from "react";
-import { Box, Button, Typography } from "@mui/material";
-import dog3 from "../assets/imgs/dog3.jpg";
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import dog3 from '../assets/imgs/dog3.jpg';
 
 const Work = () => {
   return (
-    <Box className={"section work"}>
+    <Box className={'section work'}>
       <h1>My work</h1>
       <h3>Projects</h3>
-      <Box className={"work__categories"}>
-        <button className={"category__btn active"}>
-          All<span className={"category__count"}>8</span>
+      <Box className={'work__categories'}>
+        <button className={'category__btn'}>
+          All<span className={'category__count'}>8</span>
         </button>
-        <button className={"category__btn"}>
-          FE<span className={"category__count"}>4</span>
+        <button className={'category__btn'}>
+          FE<span className={'category__count'}>4</span>
         </button>
-        <button className={"category__btn"}>
-          BE<span className={"category__count"}>4</span>
+        <button className={'category__btn'}>
+          BE<span className={'category__count'}>4</span>
         </button>
       </Box>
 
-      <Box className={"work__projects"}>
-        <a href={"#"} className={"project"} target={"_blank"}>
-          <img src={dog3} alt={""} align={"good"} className={"project__img"} />
-          <Box className={"project__description"}>
+      <Box className={'work__projects'}>
+        <a href={'#'} className={'project'} target={'_blank'}>
+          <img src={dog3} alt={''} align={'good'} className={'project__img'} />
+          <Box className={'project__description'}>
             <h3>Good Project</h3>
             <span>HTML, CSS</span>
           </Box>
         </a>
 
-        <a href={"#"} className={"project"} target={"_blank"}>
-          <img src={dog3} alt={""} align={"good"} className={"project__img"} />
-          <Box className={"project__description"}>
+        <a href={'#'} className={'project'} target={'_blank'}>
+          <img src={dog3} alt={''} align={'good'} className={'project__img'} />
+          <Box className={'project__description'}>
             <h3>Good Project</h3>
             <span>HTML, CSS</span>
           </Box>
         </a>
 
-        <a href={"#"} className={"project"} target={"_blank"}>
-          <img src={dog3} alt={""} align={"good"} className={"project__img"} />
-          <Box className={"project__description"}>
+        <a href={'#'} className={'project'} target={'_blank'}>
+          <img src={dog3} alt={''} align={'good'} className={'project__img'} />
+          <Box className={'project__description'}>
             <h3>Good Project</h3>
             <span>HTML, CSS</span>
           </Box>
         </a>
 
-        <a href={"#"} className={"project"} target={"_blank"}>
-          <img src={dog3} alt={""} align={"good"} className={"project__img"} />
-          <Box className={"project__description"}>
+        <a href={'#'} className={'project'} target={'_blank'}>
+          <img src={dog3} alt={''} align={'good'} className={'project__img'} />
+          <Box className={'project__description'}>
             <h3>Good Project</h3>
             <span>HTML, CSS</span>
           </Box>
         </a>
 
-        <a href={"#"} className={"project"} target={"_blank"}>
-          <img src={dog3} alt={""} align={"good"} className={"project__img"} />
-          <Box className={"project__description"}>
+        <a href={'#'} className={'project'} target={'_blank'}>
+          <img src={dog3} alt={''} align={'good'} className={'project__img'} />
+          <Box className={'project__description'}>
             <h3>Good Project</h3>
             <span>HTML, CSS</span>
           </Box>
